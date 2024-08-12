adce4037b431fc30a6fba6145b29c740a221c4c6
Yushis7 <150568560+Yushis7@users.noreply.github.com>
2024-08-13 06:15:17 +0900

    Add files via upload
    Signed-off-by: Yushis7 <150568560+Yushis7@users.noreply.github.com>

---
+++ calculator_in_python/calculoator2.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+from tkinter import simpledialog
+
+def add(x,y):
+    return(x+y)
+
+def subtract(x,y):
+    return(x-y)
+
+def add(x,y):
+    return(x+y)
+
+def add(x,y):
+    return(x+y)
+
+
+def calculator():
+    root = tk.Tk()
+    root.withdraw()
+
