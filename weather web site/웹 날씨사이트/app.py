5cb290827fe48caa1349dd60d7363ec3cabf8afd
Yushis7 <150568560+Yushis7@users.noreply.github.com>
2025-01-07 21:01:10 +0900

    Add files via upload
    Signed-off-by: Yushis7 <150568560+Yushis7@users.noreply.github.com>

--- weather web site/웹 날씨사이트/app.py
+++ weather web site/웹 날씨사이트/app.py
@@ -1,67 +1,34 @@
-from flask import Flask, render_template, request, jsonify, redirect, url_for
-from flask_babel import Babel, _
+from flask import Flask, render_template, request, redirect, url_for
 import requests
 
 app = Flask(__name__)
-
-# Babel 설정
-babel = Babel(app)
-app.config['BABEL_DEFAULT_LOCALE'] = 'en'
-app.config['BABEL_SUPPORTED_LOCALES'] = ['en', 'ko']
-babel = Babel(app)
+app.secret_key = "your_secret_key"
 
 # OpenWeatherMap API 키
 api_key = 'd464355f8cb80fe379285a0d20892923'
 
-# Babel의 locale 선택 함수
-def select_locale():
-    return request.accept_languages.best_match(app.config['BABEL_SUPPORTED_LOCALES'])
-
 # 즐겨찾기 리스트
 favorites = []
 
-@babel.localeselector
-def get_locale():
-    """사용자의 언어 설정을 결정"""
-    return request.accept_languages.best_match(app.config['BABEL_SUPPORTED_LOCALES'])
+# 번역 함수 대체 (직접 구현)
+def _(text):
+    return text
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    """홈 페이지 - 날씨 검색 및 즐겨찾기 관리"""
+    """홈 페이지"""
     weather_data = None
     if request.method == 'POST':  # 검색 요청
-        city_name = request.form.get('city')  # 검색창에서 입력된 도시명
-        lang = get_locale()  # 언어 설정
-        weather_url = f'http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}&units=metric&lang={lang}'
-        weather_response = requests.get(weather_url)
-        weather = weather_response.json()
+        city_name = request.form.get('city')
+        weather_data = fetch_weather(city_name)
 
-        if weather.get('cod') == 200:  # 데이터 정상 반환
-            lat = weather['coord']['lat']
-            lon = weather['coord']['lon']
-            air_pollution_url = f'http://api.openweathermap.org/data/2.5/air_pollution?lat={lat}&lon={lon}&appid={api_key}'
-            air_response = requests.get(air_pollution_url)
-            air_data = air_response.json()
+    return render_template('index.html', weather=weather_data, favorites=favorites, _=_)
 
-            pm2_5 = air_data['list'][0]['components']['pm2_5']
-            pm10 = air_data['list'][0]['components']['pm10']
-
-            weather_data = {
-                'city': city_name,
-                'temperature': weather['main']['temp'],
-                'feels_like': weather['main']['feels_like'],
-                'humidity': weather['main']['humidity'],
-                'pressure': weather['main']['pressure'],
-                'description': weather['weather'][0]['description'],
-                'lat': lat,
-                'lon': lon,
-                'pm2_5': pm2_5,
-                'pm10': pm10,
-            }
-        else:
-            weather_data = {'error': _('도시를 찾을 수 없습니다. 다시 시도해 주세요.')}
-
-    return render_template('index.html', weather=weather_data, favorites=favorites)
+@app.route('/weather/<city_name>')
+def favorite_weather(city_name):
+    """즐겨찾기 버튼 클릭 시 날씨 정보"""
+    weather_data = fetch_weather(city_name)
+    return render_template('index.html', weather=weather_data, favorites=favorites, _=_)
 
 @app.route('/add_favorite', methods=['POST'])
 def add_favorite():
@@ -78,6 +45,37 @@ def remove_favorite():
     if city_name in favorites:
         favorites.remove(city_name)
     return redirect(url_for('home'))
+
+def fetch_weather(city_name):
+    """날씨 데이터를 OpenWeatherMap에서 가져오는 함수"""
+    weather_url = f'http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}&units=metric'
+    weather_response = requests.get(weather_url)
+    weather = weather_response.json()
+
+    if weather.get('cod') == 200:
+        lat = weather['coord']['lat']
+        lon = weather['coord']['lon']
+        air_pollution_url = f'http://api.openweathermap.org/data/2.5/air_pollution?lat={lat}&lon={lon}&appid={api_key}'
+        air_response = requests.get(air_pollution_url)
+        air_data = air_response.json()
+
+        pm2_5 = air_data['list'][0]['components']['pm2_5']
+        pm10 = air_data['list'][0]['components']['pm10']
+
+        return {
+            'city': city_name,
+            'temperature': weather['main']['temp'],
+            'feels_like': weather['main']['feels_like'],
+            'humidity': weather['main']['humidity'],
+            'pressure': weather['main']['pressure'],
+            'description': weather['weather'][0]['description'],
+            'lat': lat,
+            'lon': lon,
+            'pm2_5': pm2_5,
+            'pm10': pm10,
+        }
+    else:
+        return {'error': _('City not found. Please try again.')}
 
 if __name__ == '__main__':
     app.run(debug=True)
